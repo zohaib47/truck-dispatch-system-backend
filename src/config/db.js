973cbaf84636b6aad2ca4d15a4c48280e980292e
zohaib47 <zohaibimtiaz119@gmail.com>
2026-04-27 18:58:56 +0500

    commmit

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+   // Temporarily testing ke liye
+const conn = await mongoose.connect("mongodb://zohaib_db_truck:ZohaibDB1122@ac-zljgiyr-shard-00-00.by2dn2j.mongodb.net:27017,ac-zljgiyr-shard-00-01.by2dn2j.mongodb.net:27017,ac-zljgiyr-shard-00-02.by2dn2j.mongodb.net:27017/?ssl=true&replicaSet=atlas-dj3o5l-shard-0&authSource=admin&appName=Cluster0");
     console.log(`✅ MongoDB Connected successfully: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ Error: ${error.message}`);

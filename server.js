147461b33fa540eac0d9ba8403ff7b2f95f30440
zohaib47 <zohaibimtiaz119@gmail.com>
2026-04-27 18:01:54 +0500

    commit

--- server.js
+++ server.js
@@ -31,8 +31,18 @@ app.get('/' , (req, res)=>{
     res.send('Truck Dispatching system API is running mode..')
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , ()=>{
-    console.log(`server is runing on port ${PORT}`);
-})
+// app.listen(PORT , ()=>{
+//     console.log(`server is runing on port ${PORT}`);
+// })
+
+
+// Local testing ke liye (Optional)
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+// Vercel ke liye sab se zaroori line
+module.exports = app;
